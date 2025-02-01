4517508cae193a9b83f2677ea6080e5945d26006
Matthew Stewart <catcherdbacks@msn.com>
2025-02-01 03:29:51 -0500

    add is active to the properties that need to be updated on property changed

--- src/ServiceDeskToolsApp/Models/ADUserDisplayModel.cs
+++ src/ServiceDeskToolsApp/Models/ADUserDisplayModel.cs
@@ -1,103 +1,113 @@
-﻿using System;
+﻿using EzActiveDirectory.Models;
+using System;
 using System.ComponentModel;
-using EzActiveDirectory.Models;
 
 namespace ServiceDeskToolsApp.Models
 {
-    public class ADUserDisplayModel : ActiveDirectoryUser, INotifyPropertyChanged
-    {
-        private bool _isLockedOut;
-        private bool _isExpired;
-        private bool _passwordMustBeChanged;
+	public class ADUserDisplayModel : ActiveDirectoryUser, INotifyPropertyChanged
+	{
+		private bool _isLockedOut;
+		private bool _isExpired;
+		private bool _passwordMustBeChanged;
+		private bool isActive;
 
-        public new bool IsExpired
-        {
-            get { return _isExpired; }
-            set
-            {
-                _isExpired = value;
-                CallPropertyChanged(nameof(IsExpired));
-                CallPropertyChanged(nameof(ExpiredMessage));
-            }
-        }
-        public new bool IsLockedOut
-        {
-            get { return _isLockedOut; }
-            set
-            {
-                _isLockedOut = value;
-                CallPropertyChanged(nameof(IsLockedOut));
-            }
-        }
-        public string PasswordLastSetString
-        {
-            get
-            {
-                if (PasswordLastSet.Year <= 1600)
-                {
-                    return "";
-                }
-                else
-                {
-                    return $"{ PasswordLastSet:MM/dd/yyyy h:mm tt} { (PasswordNeverExpires ? "" : $"({ PasswordExpiryDate.Subtract(DateTime.Now).Days })") }";
-                }
-            }
-        }
-        public string ExpiredMessage
-        {
-            get
-            {
-                if (IsExpired)
-                {
-                    return "(Expired)";
-                }
+		public new bool IsExpired
+		{
+			get { return _isExpired; }
+			set
+			{
+				_isExpired = value;
+				CallPropertyChanged(nameof(IsExpired));
+				CallPropertyChanged(nameof(ExpiredMessage));
+			}
+		}
+		public new bool IsActive
+		{
+			get => isActive;
+			set
+			{
+				isActive = value;
+				CallPropertyChanged(nameof(IsActive));
+			}
+		}
+		public new bool IsLockedOut
+		{
+			get { return _isLockedOut; }
+			set
+			{
+				_isLockedOut = value;
+				CallPropertyChanged(nameof(IsLockedOut));
+			}
+		}
+		public string PasswordLastSetString
+		{
+			get
+			{
+				if (PasswordLastSet.Year <= 1600)
+				{
+					return "";
+				}
+				else
+				{
+					return $"{PasswordLastSet:MM/dd/yyyy h:mm tt} {(PasswordNeverExpires ? "" : $"({PasswordExpiryDate.Subtract(DateTime.Now).Days})")}";
+				}
+			}
+		}
+		public string ExpiredMessage
+		{
+			get
+			{
+				if (IsExpired)
+				{
+					return "(Expired)";
+				}
 
-                if (AccountExpireDate is not null && AccountExpireDate?.Subtract(DateTime.Now).Days < 5)
-                {
-                    return "(Expires Soon)";
-                }
+				if (AccountExpireDate is not null && AccountExpireDate?.Subtract(DateTime.Now).Days < 5)
+				{
+					return "(Expires Soon)";
+				}
 
-                return string.Empty;
-            }
-        }
-        public bool PasswordMustBeChanged
-        {
-            get
-            {
-                if (PasswordLastSet.Year <= 1600)
-                {
-                    _passwordMustBeChanged = true;
-                }
-                else
-                {
-                    _passwordMustBeChanged = false;
-                }
+				return string.Empty;
+			}
+		}
+		public bool PasswordMustBeChanged
+		{
+			get
+			{
+				if (PasswordLastSet.Year <= 1600)
+				{
+					_passwordMustBeChanged = true;
+				}
+				else
+				{
+					_passwordMustBeChanged = false;
+				}
 
-                return _passwordMustBeChanged;
-            }
-            set
-            {
-                _passwordMustBeChanged = value;
-                if (_passwordMustBeChanged)
-                {
-                    PasswordLastSet = DateTime.MinValue;
-                }
-                else
-                {
-                    PasswordLastSet = DateTime.Now;
+				return _passwordMustBeChanged;
+			}
+			set
+			{
+				_passwordMustBeChanged = value;
+				if (_passwordMustBeChanged)
+				{
+					PasswordLastSet = DateTime.MinValue;
+				}
+				else
+				{
+					PasswordLastSet = DateTime.Now;
 					PasswordExpiryDate = PasswordLastSet.AddDays(90);
 				}
-                CallPropertyChanged(nameof(PasswordLastSet));
-                CallPropertyChanged(nameof(PasswordLastSetString));
-                CallPropertyChanged(nameof(PasswordMustBeChanged));
-            }
-        }
+				CallPropertyChanged(nameof(PasswordLastSet));
+				CallPropertyChanged(nameof(PasswordLastSetString));
+				CallPropertyChanged(nameof(PasswordMustBeChanged));
+			}
+		}
 
-        public event PropertyChangedEventHandler PropertyChanged;
+		public event PropertyChangedEventHandler PropertyChanged;
 
-        public void CallPropertyChanged(string propName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
-        }
-    }
+		public void CallPropertyChanged(string propName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+		}
+	}
 }
